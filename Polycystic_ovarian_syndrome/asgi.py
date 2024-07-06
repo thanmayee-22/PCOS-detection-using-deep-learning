18be47e1b62b24554e6993bbac9f18a7692a4ab9
Thanmayee N Shetty <thanmayeetth@gmail.com>
2024-07-06 20:17:36 +0530

    commit

---
+++ Polycystic_ovarian_syndrome/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Polycystic_ovarian_syndrome project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Polycystic_ovarian_syndrome.settings')
+
+application = get_asgi_application()
